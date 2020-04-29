44689a965c5a5595912c04734b1e8a760f80a837
MirMiao <1251267855@qq.com>
2020-04-29 20:44:39 +0800

    优化中3

---
+++ app/src/main/java/com/wd/tech/aurora/aurora.java
@@ -0,0 +1,13 @@
+package com.wd.tech.aurora;
+
+import cn.jpush.android.service.JCommonService;
+
+/**
+ * @author 吴健
+ * Class :1708A
+ * @description:
+ * @date :2020/4/29 13:53
+ * @classname :
+ */
+public class aurora extends JCommonService {
+}
